95acbc0c22929430788b133e060879b1cfc05a09
Firecraker <mezamic000@gmail.com>
2020-03-22 19:05:34 -0500

    gave crystals random values at start of game.

--- assets/javascipt/game.js
+++ assets/javascipt/game.js
@@ -1,11 +1,38 @@
+var random = 0;
+var playerScoreCounter = 0;
+var wins = 0;
+var losses = 0;
+var crystalOne = 0;
+var crystalTwo = 0;
+var crystalThree = 0;
+var crystalFour = 0;
+var lockGame = false;
+
 $(document).ready(function() {
-	var playerScoreCounter = 0;
-	var wins = 0;
-	var losses = 0;
 	$("#startBtn").on("click", function() {
 		var random = Math.floor(Math.random() * 120) + 19;
 		$("#randomNumber").text(random);
+		var playerScoreCounter = 0;
+		$("#playerScore").text(playerScoreCounter);
+		var crystalOne = Math.floor(Math.random() * 12) + 1;
+		$("#crystalOrange").text(crystalOne);
+		console.log(crystalOne);
+		var crystalTwo = Math.floor(Math.random() * 12) + 1;
+		$("#crystalYellow").text(crystalTwo);
+		console.log(crystalTwo);
+		var crystalOne = Math.floor(Math.random() * 12) + 1;
+		$("#crystalRed").text(crystalOne);
+		console.log(crystalOne);
+		var crystalOne = Math.floor(Math.random() * 12) + 1;
+		$("#crystalBlue").text(crystalOne);
+		console.log(crystalOne);
 	});
 
-	$("#crystalOne").on("click", function() {});
+	$("#crystalOrange").on("click", function() {});
+
+	$("#crystalYellow").on("click", function() {});
+
+	$("#crystalRed").on("click", function() {});
+
+	$("#crystalBlue").on("click", function() {});
 });
