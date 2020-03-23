9343772b3ac3acd4562392a216838b1d0f0f3374
Firecraker <mezamic000@gmail.com>
2020-03-23 16:17:03 -0500

    reorganized and renamed the reset function

--- assets/javascipt/game.js
+++ assets/javascipt/game.js
@@ -10,37 +10,42 @@ $(document).ready(function() {
 	var lockGame = false;
 	var counter = 0;
 
-	function start() {
+	function reset() {
+		//generates the random number and displays it
 		var targetNumber = Math.floor(Math.random() * 120) + 19;
 		$("#randomNumber").text(targetNumber);
-		//generates the random number and displays it
+
+		//starts the players score at 0 and displays it
 		var playerScoreCounter = 0;
 		$("#playerScore").text("Your Total Score Is: " + playerScoreCounter);
-		//starts the players score at 0 and displays it
+
+		//generates a random value for the orange crystal
 		var crystalOne = Math.floor(Math.random() * 12) + 1;
 		$("#crystalOrange").val(crystalOne);
 		console.log(crystalOne);
+
 		//generates a random value for the yellow crystal
 		var crystalTwo = Math.floor(Math.random() * 12) + 1;
 		$("#crystalYellow").val(crystalTwo);
 		console.log(crystalTwo);
-		//generates a random value for the yellow crystal
+
+		//generates a random value for the red crystal
 		var crystalThree = Math.floor(Math.random() * 12) + 1;
 		$("#crystalRed").val(crystalThree);
 		console.log(crystalThree);
-		//generates a random value for the red crystal
+
+		//generates a random value for the yellow crystal
 		var crystalFour = Math.floor(Math.random() * 12) + 1;
 		$("#crystalBlue").val(crystalFour);
 		console.log(crystalFour);
-		//generates a random value for the yellow crystal
-
-		$("#crystalOrange").on("click", function() {
-			crystalOne + -playerScoreCounter;
-			console.log(playerScoreCounter);
-			$("#score").text(playerScoreCounter);
-		});
 	}
-	start();
+	reset();
+
+	$("#crystalOrange").on("click", function() {
+		crystalOne + -playerScoreCounter;
+		console.log(playerScoreCounter);
+		$("#score").text(playerScoreCounter);
+	});
 
 	$(".selectCrystal").on("click", function() {
 		if (counter === targetNumber) {
@@ -49,6 +54,4 @@ $(document).ready(function() {
 	});
 
 	function clear() {}
-
-	function reset() {}
 });
