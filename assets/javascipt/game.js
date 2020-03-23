a8f57e3170d3264d8149dbfd3974c1a2a5bd9709
Firecraker <mezamic000@gmail.com>
2020-03-23 10:01:07 -0500

    trying to add scoring for crystals; bug

--- assets/javascipt/game.js
+++ assets/javascipt/game.js
@@ -1,45 +1,54 @@
-var targetNumber = 0;
-var playerScoreCounter = 0;
-var wins = 0;
-var losses = 0;
-var crystalOne = 0;
-var crystalTwo = 0;
-var crystalThree = 0;
-var crystalFour = 0;
-var lockGame = false;
-var counter = 0;
-
 $(document).ready(function() {
-	$("#startBtn").on("click", function() {
+	var targetNumber = 0;
+	var playerScoreCounter = 0;
+	var wins = 0;
+	var losses = 0;
+	var crystalOne = 0;
+	var crystalTwo = 0;
+	var crystalThree = 0;
+	var crystalFour = 0;
+	var lockGame = false;
+	var counter = 0;
+
+	function start() {
 		var targetNumber = Math.floor(Math.random() * 120) + 19;
 		$("#randomNumber").text(targetNumber);
 		//generates the random number and displays it
 		var playerScoreCounter = 0;
-		$("#playerScore").text(playerScoreCounter);
+		$("#playerScore").text("Your Total Score Is: " + playerScoreCounter);
 		//starts the players score at 0 and displays it
 		var crystalOne = Math.floor(Math.random() * 12) + 1;
-		$("#crystalOrange").text(crystalOne);
+		$("#crystalOrange").val(crystalOne);
 		console.log(crystalOne);
 		//generates a random value for the yellow crystal
 		var crystalTwo = Math.floor(Math.random() * 12) + 1;
-		$("#crystalYellow").text(crystalTwo);
+		$("#crystalYellow").val(crystalTwo);
 		console.log(crystalTwo);
 		//generates a random value for the yellow crystal
-		var crystalOne = Math.floor(Math.random() * 12) + 1;
-		$("#crystalRed").text(crystalOne);
-		console.log(crystalOne);
+		var crystalThree = Math.floor(Math.random() * 12) + 1;
+		$("#crystalRed").val(crystalThree);
+		console.log(crystalThree);
 		//generates a random value for the red crystal
-		var crystalOne = Math.floor(Math.random() * 12) + 1;
-		$("#crystalBlue").text(crystalOne);
-		console.log(crystalOne);
+		var crystalFour = Math.floor(Math.random() * 12) + 1;
+		$("#crystalBlue").val(crystalFour);
+		console.log(crystalFour);
 		//generates a random value for the yellow crystal
+	}
+	start();
+
+	$("#crystalOrange").on("click", function() {
+		playerScoreCounter += crystalOne;
+		$("#score").text(playerScoreCounter);
+		console.log(playerScoreCounter);
 	});
 
 	$(".selectCrystal").on("click", function() {
 		if (counter === targetNumber) {
 			wins++;
-		} else (counter >= targetNumber) {
-            losses--;
-        }
+		} else counter > targetNumber;
 	});
+
+	function clear() {}
+
+	function reset() {}
 });
